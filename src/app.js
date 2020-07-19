97c2a8d15b2ae3c4460791352794fa53adcf1e7b
nongguodang <982652067@qq.com>
2020-07-20 00:52:31 +0800

    引入antd并按需打包

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import React,{ Component } from 'react'
+import { Button } from 'antd'
 
 export default class App extends Component{
 	render(){
 		return(
-			<h1>hello,world!</h1>
+			<div>
+				<h1>hello,world!</h1>
+				<Button type="primary">Button</Button>
+			</div>
 		)
 	}
 }
