6878dbf07c5a1064f9bfc53d1d0c578e437d9226
农国党 <982652067@qq.com>
2020-07-21 18:07:53 +0800

    路由可用了

---
+++ src/api/request.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+//创建axios实例
+const service = axios.create({
+	baseURL: '/',
+	timeout: 6000
+})
+
+const err = (error) => {
+	if(error.response){
+		const data = error.response
+	}
+}
+
+// request interceptor
+service.interceptors.request.use(config => {
+	config.headers['token'] = 'token'
+	return config
+},err)
+
+// response interceptor
+service.interceptors.response.use((response) => {
+  return response.data
+},err)
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,10 +1,23 @@
 import React,{ Component } from 'react'
+import PubSub from 'pubsub-js'
 
-import './login.less'
+import Form from './form.jsx'
+import './login.css'
 import logo from './images/logo.png'
 /* 登录的路由组件 */
 export default class Login extends Component{
-	
+	constructor(props){
+		super(props)
+	}
+	componentDidMount(){
+		PubSub.subscribe('formValues',(msg,data)=>{
+			console.log(msg,'msg')
+			console.log(data,'data')
+		})
+	}
+	componentWillUnmount(){
+		PubSub.unsubscribe('formValues');
+	}
 	render(){
 		return(
 			<div className="login">
@@ -14,7 +27,7 @@ export default class Login extends Component{
 				</header>
 				<section className="login-content">
 					<h2>用户登录</h2>
-					
+					<Form/>
 				</section>
 			</div>
 		)
