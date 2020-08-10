2524227d1d1580be761e7fe97a0e44d108804588
nongguodang <982652067@qq.com>
2020-08-11 00:20:37 +0800

    echarts

--- src/pages/charts/index.js
+++ src/pages/charts/index.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 /* 
   图表管理
 */
-import { Table } from 'antd'
+import echarts from 'echarts'
 import ReactExcel from '../../components/excel'
 
 export default class Charts extends Component {
@@ -18,6 +18,38 @@ export default class Charts extends Component {
 			columns:columns
 		})
 	}
+	
+	initCharts = (documents,data) => {
+		let myChart = echarts.init(documents);
+		myChart.setOption(data)
+	}
+	componentDidMount(){
+		const option = {
+			title: { text: 'ECharts 入门示例', left: 'center', subtext: '二级标题' },
+			tooltip: {
+				formatter(){
+					return '这是一个提示'
+				}
+			},
+			legend: {
+				data: ['销量'],
+				bottom: 0,
+				formatter: '2020'
+			},
+			color: ['#6666FF','#990000'],
+			xAxis: {
+				data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+			},
+			yAxis: {},
+			series: {
+				name: '销量',
+				type: 'bar',
+				data: [5, 20, 36, 10, 10, 20],
+				item
+			}
+		}
+		this.initCharts(document.getElementById('main'),option)
+	}
   render() {
     return (
       <div style={{textAlign:'center',margin:20}}>
@@ -30,7 +62,7 @@ export default class Charts extends Component {
 				</ReactExcel>
 				<br/>
 				<br/>
-				<Table dataSource={this.state.dataSource} columns={this.state.columns} ></Table>
+				<div id="main" style={{width:'600px',height:'400px'}}></div>
 			</div>
     )
   }
